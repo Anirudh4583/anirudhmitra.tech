89bd783a0360693bddfaa3458767990c63db3427
Anirudh Mitra <201951024@iiitvadodara.ac.in>
2022-06-15 22:30:57 +0530

    google analytics setup

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -1,18 +1,18 @@
 import GA from './GoogleAnalytics'
-import Plausible from './Plausible'
-import SimpleAnalytics from './SimpleAnalytics'
-import Umami from './Umami'
-import siteMetadata from '@/data/siteMetadata'
+// import siteMetadata from '@/data/siteMetadata'
+// import Plausible from './Plausible'
+// import SimpleAnalytics from './SimpleAnalytics'
+// import Umami from './Umami'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 const Analytics = () => {
   return (
     <>
-      {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
+      {/* {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
       {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
-      {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
-      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
+      {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />} */}
+      {isProduction && <GA />}
     </>
   )
 }
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,30 +1,26 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  // TODO: Add site's name
+  title: 'AnirudhM[dot]Tech',
+  author: 'Anirudh Mitra',
+  // TODO: Add site's name
+  headerTitle: 'AnirudhM[dot]Tech',
+  description: "Anirudh Mitra's personal website",
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://anirudhm.tech',
+  siteRepo: 'https://github.com/anirudh4583/anirudhm.tech',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'anirudhmitra210@gmail.com',
+  github: 'https://github.com/anirudh4583',
+  twitter: 'https://twitter.com/BrownVitriol',
+  facebook: 'https://facebook.com/anirudh4583',
+  youtube: 'https://www.youtube.com/channel/UCgDh5nqxD_1rc1Z42nJRo4w/',
+  linkedin: 'https://www.linkedin.com/in/Anirudh4583',
   locale: 'en-US',
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-QE10C02KHS', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
